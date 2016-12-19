38d5f5a7f43321ec826a0b44f90b2ed18fa8bd42
micgotschall <micgotschall@Mics-MacBook-Air.local>
2016-12-19 13:43:04 -0800

    Initial Framework
    What we made at Matt D’s

---
+++ app/src/main/java/com/example/micgotschall/alolamap/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.micgotschall.alolamap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
